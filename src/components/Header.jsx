de290e9c325c46054ea775df723a3ec7544a923f
Jullian Lance Mañas <s2020925@usls.edu.ph>
2023-07-05 21:07:36 +0800

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Header(props) {
+    return(
+        <header className="header"> 
+            <img
+                src="../../images/troll-face.png"
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+        
+}
